9abf8c1f39d7e30b3e697e4af7ea20d70a48a666
striking <chris@strikinggroup.com.au>
2026-09-13 23:26:55 +1000

    Publish llms.txt with calculator and WebMCP guidance

--- app/layout.tsx
+++ app/layout.tsx
@@ -77,6 +77,7 @@ export default function RootLayout({
   return (
     <html lang="en-AU" suppressHydrationWarning>
       <head>
+        <link rel="describedby" href="/llms.txt" type="text/plain" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
         <meta name="format-detection" content="telephone=no" />
